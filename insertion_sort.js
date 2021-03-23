e56cd7dac21a519214171ee42ba586a540d5e00d
Alex Betita <alexbheb25@gmail.com>
2021-03-23 08:14:30 -0700

    simplified insertion sort

--- insertion_sort.js
+++ insertion_sort.js
@@ -1,17 +1,13 @@
-const insertion = (arr) => {
-  let min = 1
-  while (min !== arr.length) {
-      for (let i = min; i < arr.length; i++) {
-        for(let j = min - 1; j >= -1; j--){
-          if (arr[j] > arr[j + 1]) {
-            let right = arr[j+1]
-            let current = arr[j]
-            arr[j+1] = current
-            arr[j] = right
-          }
-        }
+const insertion = arr => {
+  for (let i = 1; i < arr.length; i++) {
+    for(let j = i - 1; j >= -1; j--){
+      if (arr[j] > arr[j + 1]) {
+        let right = arr[j+1]
+        let current = arr[j]
+        arr[j+1] = current
+        arr[j] = right
       }
-      min++
+    }
   }
   return arr
 }
---
+++ quick_sort.js
@@ -0,0 +1,15 @@
+const swap = (itmes, left, right) => {
+  let temp = items[left]
+  items[right] = items[left]
+  itmes[left] = temp
+}
+
+const partition = (items, left, right) => {
+  const pivot = Math.floor((left + right) / 2)
+  let leftIdx = left
+  let rightIdx = right
+
+  while(leftIdx <= rightIdx){
+    
+  }
+}
